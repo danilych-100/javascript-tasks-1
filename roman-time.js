cd4f3d4165bf461afae3463043fd0c6c158ee3ef
danilych-100 <luckyboy66@mail.ru>
2015-10-08 23:11:52 +0500

    Update roman-time.js

--- roman-time.js
+++ roman-time.js
@@ -1,4 +1,39 @@
-var hours = process.argv[2];
-var minutes = process.argv[3];
+        var hours = process.argv[2];
+        var minutes = process.argv[3];
 
-// Немного замечательного кода и магии
+        hours = parseInt(hours, 10);
+        minutes = parseInt(minutes, 10);
+
+       	if ((hours < 0 || hours > 23) || (minutes < 0 || minutes > 59)) {         
+        	console.log('Время указано не верно');
+            }
+            else {
+            	console.log(ConvertToRomans(hours) +':'+ ConvertToRomans(minutes));
+            }
+function ConvertToRomans(time)
+        {
+            var countOfTens = '';
+            var number = time;
+            var romansNumbers = ['','I','II','III','IV','V','VI','VII','VIII','IX','X','L']   
+            if (time == 0)
+            {
+                romansNumbers[0] = "nn";
+            }         
+            for (i = 1; i <= (time / 10); i++)
+            {
+                countOfTens += romansNumbers[10];
+            }          
+            if ((time % 10 == 9)&&(time<30)&&(time > 10))
+            {
+                return number = countOfTens + romansNumbers[9];
+            }
+            if (time / 10==4)
+            {
+                return number = romansNumbers[10] + romansNumbers[11] + romansNumbers[time % 10];
+            }
+            if (time / 10 == 5)
+            {
+                return number= romansNumbers[11] + romansNumbers[time % 10];
+            }
+            return number = countOfTens + romansNumbers[time % 10];
+        }       
